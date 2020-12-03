abd9176f95338e03e96f43c85852bf8e8128385b
Thex7 <srthex7@gmail.com>
2020-12-03 14:35:43 -0500

    Test commit

---
+++ jologram/src/main/java/work/srthex7/mcraft/commons/jologram/App.java
@@ -0,0 +1,13 @@
+package work.srthex7.mcraft.commons.jologram;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ jologram/src/test/java/work/srthex7/mcraft/commons/jologram/AppTest.java
@@ -0,0 +1,20 @@
+package work.srthex7.mcraft.commons.jologram;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
